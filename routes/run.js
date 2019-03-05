d0891382e572eaae7b85be4fbabf75da33f3f840
Ryan Purcell <ryan.purcell@gexaenergy.com>
2019-03-05 16:38:43 -0600

    feat(fact_run): Added new endpoint for fact run
    Added new endpoint that takes in a ruleset id and fact and returns a
    result.  Added json file for test data and lodash to be able to search
    and find records.

--- routes/run.js
+++ routes/run.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const rules_helper = require('../lib/rule.helper');
-const validation_middleware = require('../lib/validation.helper').validation_middleware('evaluation_request');
+const validation_helper = require('../lib/validation.helper');
+const evaluation_request_validation_middleware = validation_helper.validation_middleware('evaluation_request');
+const fact_run_request_validation_middleware = validation_helper.validation_middleware('fact_run_request');
+const rulesets = require('../data/rule_sets');
+const _ = require('lodash');
 
 router.post('/',
-    validation_middleware,
+    evaluation_request_validation_middleware,
     async function (req, res, next) {
         try {
             let request = req.body; //req.evaluation_request
@@ -15,14 +19,27 @@ router.post('/',
         }
     });
 
-// router.post('/ruleset', function (req, res, next) {
-//     let valid = validate_rule_set(req.body);
-//     if (valid) {
-//         res.send(req.body);
-//     }
-//     else {
-//         res.send(ajv.errorsText(validate_rule_set.errors));
-//     }
-// });
+router.post('/ruleset/:rule_set_id',
+    fact_run_request_validation_middleware,
+    async function (req, res, next) {
+        try {
+            let rule_set_id = parseInt(req.params.rule_set_id);
+            let fact = req.fact_run_request.fact;
+            let first_result_only = req.fact_run_request.first_result_only;
+            let ruleset = _.find(rulesets, function (rs) {
+                return rs.id === rule_set_id;
+            });
+            if (ruleset) {
+                let result = await rules_helper.evaluate(ruleset.rules, fact, first_result_only);
+                res.send(result);
+            }
+            else {
+                res.sendStatus(404);
+            }
+        }
+        catch (err) {
+            next(err);
+        }
+    });
 
 module.exports = router;
