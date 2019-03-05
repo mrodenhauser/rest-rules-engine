ae3a9be12a3d291bf6ddc4b9044d9b8f48fe1c1b
Ryan Purcell <ryan.purcell@gexaenergy.com>
2019-03-05 13:49:37 -0600

    feat(rule_eval): Added method for evaluating rules against a fact
    Added endpoint that takes in rules and fact and evaluates a result.
    Added validation on endpoint based off of swagger definition.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let enginesRouter = require('./routes/engine');
-let indexRouter = require('./routes/index');
+
+let swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./specs/swagger.json');
 
 let app = express();
 
@@ -15,28 +17,41 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/docs/swagger', function (req, res) {
+    swaggerDocument.servers[0].url = 'http://' + req.get('host');
+    res.send(swaggerDocument)
+});
+
 app.use('/run', require('./routes/run'));
 app.use('/engines/', enginesRouter);
-app.use('/', indexRouter);
+
+app.use('/', swaggerUi.serve);
+app.get('/',
+    function (req, res) {
+        swaggerDocument.servers[0].url = 'http://' + req.get('host');
+        swaggerUi.setup(swaggerDocument)(req, res);
+    },
+    swaggerUi.setup(swaggerDocument)
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.send(err.message);
 });
 
 module.exports = app;
--- routes/run.js
+++ routes/run.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
 const rules_helper = require('../lib/rule.helper');
+const validation_middleware = require('../lib/validation.helper').validation_middleware('evaluation_request');
 
 router.post('/',
-    //Validation middleware will validate input and set req.evaluation_request from body
+    validation_middleware,
     async function (req, res, next) {
         try {
             let request = req.body; //req.evaluation_request
-            let result = await rules_helper.evaluate(request.rules, request.facts, request.first_result_only);
+            let result = await rules_helper.evaluate(request.rules, request.fact, request.first_result_only);
             res.send(result);
         }
         catch (err) {
             next(err);
         }
     });
+
+// router.post('/ruleset', function (req, res, next) {
+//     let valid = validate_rule_set(req.body);
+//     if (valid) {
+//         res.send(req.body);
+//     }
+//     else {
+//         res.send(ajv.errorsText(validate_rule_set.errors));
+//     }
+// });
 
 module.exports = router;
