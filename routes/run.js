7921d763a9b6b5e231054bc1d919bbebd3826106
ryan.purcell <patrick.ryan.purcell@gmail.com>
2019-03-25 13:55:04 -0500

    fix(run): Changed the run endpoint for id to pull from the mongodb store instead of local files

--- routes/run.js
+++ routes/run.js
@@ -5,8 +5,6 @@ const rule_set_service = require('../lib/services/rule_set.service');
 const evaluation_request_validation_middleware = validation_helper.validation_middleware('evaluation_request');
 const fact_run_request_validation_middleware = validation_helper.validation_middleware('fact_run_request');
 const fact_rule_name_request_validation_middleware = validation_helper.validation_middleware('fact_rule_name_request');
-const rulesets = require('../data/rule_sets');
-const _ = require('lodash');
 
 router.post('/',
     evaluation_request_validation_middleware,
@@ -25,12 +23,10 @@ router.post('/ruleset/:rule_set_id',
     fact_run_request_validation_middleware,
     async function (req, res, next) {
         try {
-            let rule_set_id = parseInt(req.params.rule_set_id);
+            let rule_set_id = req.params.rule_set_id;
             let fact = req.fact_run_request.fact;
             let first_result_only = req.fact_run_request.first_result_only;
-            let ruleset = _.find(rulesets, function (rs) {
-                return rs.id === rule_set_id;
-            });
+            let ruleset = await rule_set_service.getById(rule_set_id);
             if (ruleset) {
                 let result = await rules_helper.evaluate(ruleset.rules, fact, first_result_only);
                 res.send(result);
