c58d2afc97e0cfdcce42042c28527e92049ce125
ryan.purcell <patrick.ryan.purcell@gmail.com>
2019-03-25 13:55:39 -0500

    fix(logs): Fixed logging to log ip and also return 400 on known errors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('log-driver')({ level: "trace" });
+const logger = require('log-driver')({level: "trace"});
 const morgan = require('morgan');
 
 let enginesRouter = require('./routes/engine');
@@ -49,21 +49,33 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
+    let ip = (req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress).split(",")[0];
+
     let error_details = {
         message: err.message,
         stack: err.stack,
         error: err,
+        request_ip: ip,
         request_headers: req.headers,
         request_url: req.url,
         request_params: req.params,
         request_query: req.query,
         request_body: req.body
     };
-    logger.error(JSON.stringify(error_details));
 
-    // render the error page
-    res.status(err.status || 500);
-    res.send(err.message || err);
+    if (err.code) {
+        logger.warn(JSON.stringify(error_details));
+        res.status(400);
+        res.send([err.message]);
+    }
+    else {
+        logger.error(JSON.stringify(error_details));
+        res.status(err.status || 500);
+        res.send(err.message || err);
+    }
 });
 
 module.exports = app;
